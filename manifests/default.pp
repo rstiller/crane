cd87a77a48e9fa476b06b60aa302c612664aaa0d
rstiller <robert.stiller.mail@googlemail.com>
2013-09-08 14:35:02 +0200

    vagrant provisioning for docker and lxc

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,9 +1,16 @@
 
-exec { "apt-get update":
+exec { 'apt-get update':
     user => root,
-    path => [ "/usr/bin/" ],
+    path => [ '/usr/bin/' ],
 } ->
 
-package { ['ruby1.9.3', 'make', 'git', 'sqlite3', 'libsqlite3-ruby1.9.1']:
+package { ['ruby1.9.3', 'make', 'git', 'sqlite3', 'libsqlite3-ruby1.9.1', 'lxc']:
     ensure => latest,
+} ->
+
+exec { 'wget --output-document=docker https://get.docker.io/builds/Linux/x86_64/docker-latest && chmod +x /usr/bin/docker':
+    cwd     => '/usr/bin',
+    user    => root,
+    path    => [ '/usr/bin/', '/bin' ],
+    creates => '/usr/bin/docker',
 }
