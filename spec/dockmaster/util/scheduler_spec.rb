a61e54a30ffb32876155cfde4711ee5d9cc1a9b3
rstiller <robert.stiller.mail@googlemail.com>
2013-09-21 13:33:48 +0200

    added scheduler.checkProject tests

--- spec/dockmaster/util/scheduler_spec.rb
+++ spec/dockmaster/util/scheduler_spec.rb
@@ -32,7 +32,77 @@ describe "Scheduler" do
     
     context "checkProject" do
         
-        it "nothing" do
+        it "check proc submission - check tags" do
+            
+            branches = {}
+            tags = {}
+            url = "url"
+            project = Dockmaster::Models::Project.new
+            pool = Dockmaster::ThreadPool.new 1
+            func = Proc.new do
+            end
+            originalFunc = nil
+            
+            expect(Proc).to receive(:new) do |*args, &block|
+                originalFunc = block
+                func
+            end
+            
+            expect(Dockmaster::ThreadPool).to receive(:new).twice.and_return(pool)
+            expect(pool).to receive(:submit).with(func)
+            
+            scheduler = Dockmaster::Scheduler.new 2, 4
+            scheduler.checkProject project
+            
+            expect(originalFunc).not_to eq(nil)
+            
+            expect(project).to receive(:url).twice.and_return(url)
+            expect(project).to receive(:buildsTags?).and_return(true)
+            
+            expect(Dockmaster::Git).to receive(:remoteTags).with(url).and_return(tags)
+            expect(scheduler).to receive(:checkTags).with(project, tags)
+            
+            expect(Dockmaster::Git).to receive(:remoteBranches).with(url).and_return(branches)
+            expect(scheduler).to receive(:checkBranches).with(project, branches)
+            
+            originalFunc.call
+            
+        end
+        
+        it "check proc submission - don't check tags" do
+            
+            branches = {}
+            url = "url"
+            project = Dockmaster::Models::Project.new
+            pool = Dockmaster::ThreadPool.new 1
+            func = Proc.new do
+            end
+            originalFunc = nil
+            
+            expect(Proc).to receive(:new) do |*args, &block|
+                originalFunc = block
+                func
+            end
+            
+            expect(Dockmaster::ThreadPool).to receive(:new).twice.and_return(pool)
+            expect(pool).to receive(:submit).with(func)
+            
+            scheduler = Dockmaster::Scheduler.new 2, 4
+            scheduler.checkProject project
+            
+            expect(originalFunc).not_to eq(nil)
+            
+            expect(project).to receive(:url).once.and_return(url)
+            expect(project).to receive(:buildsTags?).and_return(false)
+            
+            expect(Dockmaster::Git).not_to receive(:remoteTags)
+            expect(scheduler).not_to receive(:checkTags)
+            
+            expect(Dockmaster::Git).to receive(:remoteBranches).with(url).and_return(branches)
+            expect(scheduler).to receive(:checkBranches).with(project, branches)
+            
+            originalFunc.call
+            
         end
         
     end
