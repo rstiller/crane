f25d6230d82543e35d1146c5dc4a72b13773c29b
rstiller <robert.stiller.mail@googlemail.com>
2013-10-09 22:49:44 +0200

    project github owner picture + project menu styling

--- src/app/projects/menu.js
+++ src/app/projects/menu.js
@@ -1,10 +1,38 @@
 
-angular.module('dashboard.controllers').controller('ProjectsMenuCtrl', ['$scope', 'Projects', function($scope, Projects) {
+angular.module('dashboard.controllers').controller('ProjectsMenuCtrl', ['$scope', '$http', 'Projects', function($scope, $http, Projects) {
     
     $scope.data = {};
     $scope.data.newProject = {
         'name': '',
         'url': ''
+    };
+    
+    var refresh = function() {
+        Projects.query({}, function(projects) {
+            
+            for(var i = 0; i < projects.elements.length; i++) {
+                
+                var project = projects.elements[i];
+                var repo = project.url.split('https://github.com/')[1];
+                
+                project.imageUrl = '';
+                
+                $http.jsonp('https://api.github.com/repos/' + repo + '?callback=JSON_CALLBACK').success(function(response) {
+                    
+                    angular.forEach($scope.data.projects.elements, function(project) {
+                        
+                        if(project.url == response.data.html_url) {
+                            project.imageUrl = response.data.owner.avatar_url;
+                        }
+                        
+                    });
+                    
+                });
+                
+            }
+            
+            $scope.data.projects = projects;
+        });
     };
     
     $scope.save = function() {
@@ -13,22 +41,16 @@ angular.module('dashboard.controllers').controller('ProjectsMenuCtrl', ['$scope'
                 'name': '',
                 'url': ''
             };
-            Projects.query({}, function(projects) {
-                $scope.data.projects = projects;
-            });
+            refresh();
         });
     };
     
     $scope.remove = function(id) {
         Projects.remove({ 'id': id }, function() {
-            Projects.query({}, function(projects) {
-                $scope.data.projects = projects;
-            });
+            refresh();
         });
     };
     
-    Projects.query({}, function(projects) {
-        $scope.data.projects = projects;
-    });
+    refresh();
     
 }]);
