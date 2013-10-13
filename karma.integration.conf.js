679a7a2e1cc1ec73f808cbb5bc4cf26b5b795704
rstiller <robert.stiller.mail@googlemail.com>
2013-10-13 21:12:16 +0200

    test setup

--- karma.integration.conf.js
+++ karma.integration.conf.js
@@ -18,7 +18,7 @@ module.exports = function(config) {
         // - Safari (only Mac)
         // - PhantomJS
         // - IE (only Windows)
-        browsers : [ 'PhantomJS', 'Firefox' ],
+        browsers : [ 'PhantomJS' ],
         captureTimeout : 60000,
         singleRun : true
     });
