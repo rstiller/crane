08d0594ce747294603988a23e24d889401c489d0
rstiller <robert.stiller.mail@googlemail.com>
2013-10-24 22:01:17 +0200

    new version / environment / service visualisation

--- src/app/projects/detail.js
+++ src/app/projects/detail.js
@@ -3,65 +3,54 @@ angular.module('dashboard.controllers').controller('ProjectDetailCtrl', ['$scope
                                                                          function($scope, $stateParams, Projects, Cache) {
     
     $scope.data = {};
-    $scope.diagramData = null;
-    $scope.data.currentVersion = null;
+    $scope.data.selectedEnvironment = '';
+    $scope.data.selectedVersion = {};
+    $scope.data.versions = [];
+    $scope.data.environments = [];
     
-    $scope.selectVersion = function(version) {
-        $scope.data.currentVersion = version;
-        
-        var data = {};
-        
-        data.name = version.name;
-        data.children = [];
-        
-        angular.forEach(version.runConfigs, function(config) {
-            var exists = false;
-            angular.forEach(data.children, function(envConfig) {
-                if(envConfig.name === config.environment) {
-                    exists = true;
-                }
-            });
-            if(exists === false) {
-                data.children.push({
-                    'name': config.environment,
-                    'children': []
-                });
-            }
-        });
-        
-        angular.forEach(version.runConfigs, function(config) {
-            angular.forEach(data.children, function(envConfig) {
-                if(envConfig.name === config.environment) {
-                    envConfig.children.push({
-                        'name': config.serviceName,
-                        'size': 1
-                    });
-                }
-            });
-        });
-        
-        if(version.runConfigs.length > 0) {
-            $scope.diagramData = data;
-        }
-        
+    $scope.selectEnvironment = function(environment) {
+        $scope.data.selectedEnvironment = environment;
     };
+    
+    $scope.$watch('data.selectedVersion', function() {
+        var environmentNames = {};
+        angular.forEach($scope.data.selectedVersion.runConfigs, function(runConfig) {
+            environmentNames[runConfig.environment] = environmentNames[runConfig.environment] || [];
+            environmentNames[runConfig.environment].push(runConfig.serviceName);
+        });
+        var environemnts = [];
+        angular.forEach(environmentNames, function(value, key) {
+            environemnts.push({
+                'name': key,
+                'services': value
+            });
+        });
+        $scope.data.environments = environemnts;
+        $scope.data.selectedEnvironment = '';
+    });
     
     Projects.get({
         'id': $stateParams.projectId
     }, function(project) {
         
         $scope.data.project = project;
-        $scope.data.versions = [];
         
         Cache.get(project.url, function(data) {
         });
         
+        var versions = [];
+        
         angular.forEach(project.branches, function(branch) {
-            $scope.data.versions.push(branch)
+            versions.push(branch);
+            if(branch.name === 'master') {
+                $scope.data.selectedVersion = branch;
+            }
         });
         angular.forEach(project.tags, function(tag) {
-            $scope.data.versions.push(tag)
+            versions.push(tag);
         });
+        
+        $scope.data.versions = versions;
         
     });
     
