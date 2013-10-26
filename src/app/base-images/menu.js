4a411327f9c3a4a106855b888e39b8d0919d47af
rstiller <robert.stiller.mail@googlemail.com>
2013-10-26 03:29:47 +0200

    base images menu (list, filter, pull standard images, remove)

--- src/app/base-images/menu.js
+++ src/app/base-images/menu.js
@@ -1,3 +1,43 @@
 
-angular.module('dashboard.controllers').controller('BaseImagesMenuCtrl', ['$scope', function($scope) {
+angular.module('dashboard.controllers').controller('BaseImagesMenuCtrl', ['$rootScope', '$scope', '$location', 'BaseImages',
+                                                                          function($rootScope, $scope, $location, BaseImages) {
+    
+    $scope.data = {};
+    $scope.data.baseImages = [];
+    $scope.data.pullBaseImage = {
+        'name': ''
+    };
+    
+    var refresh = function() {
+        
+        BaseImages.query({}, function(baseImages) {
+            $scope.data.baseImages = baseImages;
+        });
+        
+    };
+    
+    $scope.pullImage = function() {
+        BaseImages.pull({
+            'id': $scope.data.pullBaseImage.name
+        }, function() {
+            $scope.data.pullBaseImage.name = '';
+            refresh();
+        });
+    };
+    
+    $scope.openDialog = function() {
+    };
+    
+    $scope.remove = function(id) {
+        BaseImages.remove({ 'id': id }, function() {
+            refresh();
+        });
+        
+        if($rootScope.$stateParams.baseImageId == id) {
+            $location.path('/base-images/');
+        }
+    };
+    
+    refresh();
+    
 }]);
