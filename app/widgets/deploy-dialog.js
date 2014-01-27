f2960d2b14a8b739bdd57ce45b03e84cb2783752
rstiller <robert.stiller.mail@googlemail.com>
2014-01-27 22:51:03 +0100

    choose group to deploy to and submit deploy job

--- app/widgets/deploy-dialog.js
+++ app/widgets/deploy-dialog.js
@@ -1,29 +1,52 @@
 
 angular.module('dashboard.widgets').controller('DeployDialogCtrl',
-    ['$scope', 'Project', 'DeployJob', function($scope, Project, DeployJob) {
+    ['$scope', 'Project', 'MachineGroup', 'DeployJob', function($scope, Project, MachineGroup, DeployJob) {
 
     $scope.cssClass = 'deploy-dialog';
     $scope.data = {};
     $scope.data.ready = false;
+    $scope.data.group = null;
 
     $scope.$watch('version', function(revision) {
         var workingCopy = Project.getWorkingCopy($scope.project, revision);
         $scope.data.version = workingCopy.name;
     });
+    
+    $scope.init = function() {
+    	MachineGroup.all({
+            error: function(err) {
+                console.log(err);
+            },
+            success: function(groups) {
+                $scope.data.groups = groups;
+                if(!!groups && groups.length > 0) {
+                	$scope.data.group = groups[0];
+                }
+                $scope.data.ready = true;
+                $scope.$apply();
+            }
+        });
+    };
 
     $scope.deploy = function() {
         var workingCopy = Project.getWorkingCopy($scope.project, $scope.version);
-        // TODO: choose machine-group
         var deployJob = new DeployJob({
             'projectId': $scope.project.get('_id'),
             'workingCopyName': workingCopy.name,
             'workingCopyType': workingCopy.type,
             'workingCopyRev': workingCopy.rev,
-            'machineGroupId': null,
+            'machineGroupId': $scope.data.group.get('_id'),
             'service': $scope.service,
             'environment': $scope.environment
         });
         deployJob.save({
+        	error: function(err) {
+        		console.log(err);
+        	},
+        	success: function() {
+        		$scope.closeDialog();
+                $scope.$apply();
+        	}
         });
     };
 
